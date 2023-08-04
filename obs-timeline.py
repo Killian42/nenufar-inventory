290601b4f880c345d384dbd808b71818be4f336d
Killian Lebreton <killian.lebreton35@gmail.com>
2023-08-04 18:50:19 +0200

    better formatting and title tweaks

--- obs-timeline.py
+++ obs-timeline.py
@@ -20,8 +20,9 @@ import glob
 import os
 import argparse
 import subprocess
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 matplotlib.rcParams["font.size"] = 17
+
 
 ### Argument parsing ###
 def parse_args():
@@ -44,7 +45,7 @@ def parse_args():
         "--scope",
         dest="scope",
         type=str,
-        choices=["all","thisweek"],
+        choices=["all", "thisweek"],
         default="all",
         help="Determine which observations are considered",
     )
@@ -54,7 +55,7 @@ def parse_args():
         "--email",
         dest="email",
         type=str,
-        nargs='+',
+        nargs="+",
         default=None,
         help="Send the pdf file to this email adresss (or list of emails adresses).",
     )
@@ -63,19 +64,20 @@ def parse_args():
 
     return args
 
+
 args = parse_args()
 
 ### Data importation and preparation ###
-file=glob.glob(os.getcwd() + "/pulsar-obs-inventory*.csv")[-1]
+file = glob.glob(os.getcwd() + "/pulsar-obs-inventory*.csv")[-1]
 df = pd.read_csv(file, sep=";")
-actual_psr_mask = (df["Pulsar name"].str[0]=="B") | (df["Pulsar name"].str[0]=="J")
+actual_psr_mask = (df["Pulsar name"].str[0] == "B") | (df["Pulsar name"].str[0] == "J")
 df = df[actual_psr_mask]
 
-if args.scope=='thisweek':
+if args.scope == "thisweek":
     today = datetime.now()
-    lastweek = today-timedelta(7)
-    lastweek_str =  lastweek.strftime("%Y-%m-%d")
-    t=Time(lastweek_str, format='isot', scale='utc')
+    lastweek = today - timedelta(7)
+    lastweek_str = lastweek.strftime("%Y-%m-%d")
+    t = Time(lastweek_str, format="isot", scale="utc")
     cut_off = t.mjd
 
     mask = df["Filename"].str.split("_").str[2].astype(dtype=float) > cut_off
@@ -97,12 +99,13 @@ psrs = df["Pulsar name"]
 unique_psrs = df["Pulsar name"].unique()
 unique_psrs_sorted = unique_psrs[np.argsort(df["Pulsar name"].str[1:].unique())]
 
-x0=df["obs_day_mjd"].min()-x_lim_pad
-x1=df["obs_day_mjd"].max()+x_lim_pad
-x_lims = (x0,x1)
-x_lims_utc = ((Time(x0,format="mjd").datetime64,Time(x1,format="mjd").datetime64))
+x0 = df["obs_day_mjd"].min() - x_lim_pad
+x1 = df["obs_day_mjd"].max() + x_lim_pad
+x_lims = (x0, x1)
+x_lims_utc = (Time(x0, format="mjd").datetime64, Time(x1, format="mjd").datetime64)
 
-def timeline_mkr(psr_arr,disp=False,padding=pad):
+
+def timeline_mkr(psr_arr, disp=False, padding=pad):
     fig = plt.figure(figsize=(12.4, 10.4))
     ax1 = fig.add_subplot()
     ax2 = ax1.twiny()
@@ -115,33 +118,33 @@ def timeline_mkr(psr_arr,disp=False,padding=pad):
         col = next(colors)["color"]
 
         ax1.text(
-        df["obs_day_mjd"].min()-padding,
-        i,
-        s=psr,
-        c=col,
-        fontsize=10,
-        horizontalalignment="right",
-        verticalalignment="center",
-        zorder=5,
-    )
+            df["obs_day_mjd"].min() - padding,
+            i,
+            s=psr,
+            c=col,
+            fontsize=10,
+            horizontalalignment="right",
+            verticalalignment="center",
+            zorder=5,
+        )
         sc = ax1.scatter(
-        sel["obs_day_mjd"],
-        ys,
-        c=col,
-        marker="x",
-        s=25,
-        lw=1,
-        zorder=5,
-    )
+            sel["obs_day_mjd"],
+            ys,
+            c=col,
+            marker="x",
+            s=25,
+            lw=1,
+            zorder=5,
+        )
         sc2 = ax2.scatter(
-        sel["utc"],
-        ys,
-        alpha=0,
-    )
-    
+            sel["utc"],
+            ys,
+            alpha=0,
+        )
+
     ax1.grid()
     ax1.set_xlabel("MJD")
-    ax1.set_ylabel("Pulsar name",labelpad=+75)
+    ax1.set_ylabel("Pulsar name", labelpad=+75)
     ax1.set(yticklabels=[])
     ax1.tick_params(left=False)
     ax1.set_xlim(x_lims)
@@ -157,25 +160,30 @@ def timeline_mkr(psr_arr,disp=False,padding=pad):
     ax2.xaxis.set_major_locator(locator)
     ax2.xaxis.set_major_formatter(mdates.ConciseDateFormatter(locator))
 
-    plt.xticks(rotation=10)
+    plt.title(
+        "NenuFAR pulsar observations timeline of " + psr_arr[-1] + " to " + psr_arr[0],
+        pad=20,
+        size=20,
+    )
 
-    plt.title("Nenufar pulsar observations timeline of " + psr_arr[-1] + " to " + psr_arr[0],pad=8,size=20)   
-
-    pdf.savefig()  # saves the current figure into a pdf page  
+    pdf.savefig()  # saves the current figure into a pdf page
     plt.close()
 
-nb_of_arr = int(len(unique_psrs_sorted)/50)+1
-split_arr = np.array_split(unique_psrs_sorted,nb_of_arr)
 
-with PdfPages(os.getcwd() +'/obs_timeline_'+args.scope+'.pdf') as pdf:
+nb_of_arr = int(len(unique_psrs_sorted) / 50) + 1
+split_arr = np.array_split(unique_psrs_sorted, nb_of_arr)
+
+with PdfPages(os.getcwd() + "/obs_timeline_" + args.scope + ".pdf") as pdf:
     for arr in split_arr:
         timeline_mkr(np.flip(arr))
 
 if args.email is not None:
     content = "This is an email your requested regarding NenuFAR pulsar observations. \n Please find attached a pdf with a timeline of these observations."
     subject = "NenuFAR pulsar observation timeline"
-    attachment_path = os.getcwd() +'/obs_timeline_'+args.scope+'.pdf'
+    attachment_path = os.getcwd() + "/obs_timeline_" + args.scope + ".pdf"
     recipients = args.email
 
-    command = 'echo "{}" | mail -s "{}" -A {} {}'.format(content,subject, attachment_path, ','.join(recipients))
-    subprocess.run(command,shell=True)
+    command = 'echo "{}" | mail -s "{}" -A {} {}'.format(
+        content, subject, attachment_path, ",".join(recipients)
+    )
+    subprocess.run(command, shell=True)
